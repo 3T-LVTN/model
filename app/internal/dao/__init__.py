096b07fce700d00ac65be5ba5f467788ec70bc1d
thinh.lth <thinh.lth@teko.vn>
2023-05-01 16:06:29 +0700

    fix missing table

--- app/internal/dao/__init__.py
+++ app/internal/dao/__init__.py
@@ -1,3 +1,5 @@
 from app.internal.dao.location import Location
 from app.internal.dao.time_window import TimeWindow
 from app.internal.dao.weather_log import WeatherLog
+from app.internal.dao.predicted_var import PredictedVar
+from app.internal.dao.predicted_log import PredictedLog
