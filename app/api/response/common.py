b3accb28c26dc5b5deadfc2efdf6a52be4b611de
thinh.lth <thinh.lth@teko.vn>
2023-06-02 14:52:05 +0700

    fix enum invalid value

--- app/api/response/common.py
+++ app/api/response/common.py
@@ -4,8 +4,8 @@ from enum import Enum
 class Rate(str, Enum):
     SAFE = "SAFE"
     NORMAL = "NORMAL"
-    LOW_RISK = "LOW RISK"
-    HIGH_RISK = "HIGH RISK"
+    LOW_RISK = "LOW_RISK"
+    HIGH_RISK = "HIGH_RISK"
 
 
 MAP_IDX_TO_RATE = {idx: value for idx, value in enumerate(Rate.__members__)}
